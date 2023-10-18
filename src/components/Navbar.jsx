5a7a0a6ad649a6c58c66c7ec4a36b1fa72e08df8
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-10-18 21:40:27 +0530

    Created the layout for the app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { styles } from "../style";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [active, setActive] = useState("");
+
+  return (
+    <nav
+      className={`${styles.paddingX} w-full items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-start gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
